5f9423b956491c07bc8c5aef2233f8b24cb79dac
Brian Kurosawa <brian.kurosawa@gmail.com>
2021-09-29 19:34:21 -0300

    First commit

---
+++ src/main/java/br/com/devtudo/devprobanco/DevprobancoApplication.java
@@ -0,0 +1,13 @@
+package br.com.devtudo.devprobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevprobancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevprobancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devtudo/devprobanco/DevprobancoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devtudo.devprobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevprobancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
