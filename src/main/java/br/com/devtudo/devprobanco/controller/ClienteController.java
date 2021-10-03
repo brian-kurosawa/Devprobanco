ece34061d5ea9898df2b4e9fcbdb7cd3291a1d63
renatoGalloAlves <renato_gallo_20@hotmail.com>
2021-10-03 20:26:07 -0300

    finalizado

--- src/main/java/br/com/devtudo/devprobanco/controller/ClienteController.java
+++ src/main/java/br/com/devtudo/devprobanco/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.devtudo.devprobanco.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import br.com.devtudo.devprobanco.DAO.ClienteDao;
 import br.com.devtudo.devprobanco.model.Cliente;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ClienteController {
 	@Autowired
 	ClienteDao dao;
--- src/main/java/br/com/devtudo/devprobanco/controller/ContaController.java
+++ src/main/java/br/com/devtudo/devprobanco/controller/ContaController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import br.com.devtudo.devprobanco.DAO.ContaDao;
 import br.com.devtudo.devprobanco.model.Conta;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ContaController {
 	@Autowired
 	ContaDao dao;
